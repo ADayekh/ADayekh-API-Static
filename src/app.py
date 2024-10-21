ad7c757b26a2fb3377d6f3f366ed4060b89b72a1
Alejandro Dayekh García <maestrodayekh@gmail.com>
2024-10-21 18:21:46 +0000

    ¿Funcional? Puede tener algún fallo

--- src/app.py
+++ src/app.py
@@ -15,6 +15,30 @@ CORS(app)
 # create the jackson family object
 jackson_family = FamilyStructure("Jackson")
 
+John = {
+    "first_name": "John",
+    "age": 33,
+    "lucky_numbers": [7, 13, 22]
+}
+
+Jane = {
+    "first_name": "Jane",
+    "age": 35,
+    "lucky_numbers": [10, 14, 3]
+}
+
+Jimmy = {
+    "first_name": "Jimmy",
+    "age": 5,
+    "lucky_numbers": [1]
+}
+
+jackson_family.add_member(John)
+jackson_family.add_member(Jane)
+jackson_family.add_member(Jimmy)
+
+
+
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
@@ -26,19 +50,47 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
-
+def get_members():
     # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+    if members is not None:
+        return jsonify(members), 200
+    else:
+        return "Not members found", 404
 
 
-    return jsonify(response_body), 200
 
-# this only runs if `$ python src/app.py` is executed
+@app.route('/member/<int:id>', methods=['GET'])
+def obtain_member(id):
+    member = jackson_family.get_member(id)
+    if member is not None:
+        return jsonify(member), 200
+    else:
+        return "Not member found", 404
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    member = request.get_json()
+    if 'first_name' not in member :
+        return "No name", 400
+    if 'age' not in member:
+        return "No age", 400
+    if 'lucky_numbers' not in member:
+        return "No numbers", 400
+    print("Member added", member)
+    jackson_family.add_member(member)
+    if member is not None:
+        return "Member created", 200
+
+
+
+@app.route('/member/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    jackson_family.delete_member(id)
+    return "Member deleted", 200
+
+
+
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=True)
